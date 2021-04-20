a31b937c3ff139ab5cfd2d338a6e3da89d64d406
mikeNegron <mikenegron@gmail.com>
2021-04-20 17:11:07 -0400

    Fifth Commit

--- Snek.py
+++ Snek.py
@@ -1,14 +1,15 @@
 from graphics import*
 from random import*
 
-#Variables used:
+# Variables used:
 WIDTH = 400
 HEIGHT = 470
 GRID_HEIGHT = WIDTH
+FRAMES = 5
 SCORE = 0
 X = 30
 Y = 70
-i = 0
+BODY = 0
 RADIUS = 10
 LENGTH = RADIUS * 2
 PLAYER_LENGTH = 3
@@ -18,30 +19,36 @@ OVER = False
 SPAWN = False
 REWARD = 0
 
-#Reward coordinates:
-REWARD_X = randrange(0, WIDTH - 20, 20)
-REWARD_Y = randrange(0, GRID_HEIGHT - 20, 20)
-
-#Main GUI:
+# Main GUI:
 WIN = GraphWin("SNEK", WIDTH, HEIGHT, autoflush=False)
 WIN.setBackground(color_rgb(15, 15, 15))
 
-#Game defs:
+# Game defs:
 def grid(WIDTH, GRID_HEIGHT):
     for i in range(20, WIDTH, 20):
         GRID_VERTICAL = Line(Point(i, 0), Point(i, GRID_HEIGHT))
         GRID_VERTICAL.setOutline(color_rgb(25, 25, 25))
         GRID_VERTICAL.draw(WIN)
-        
+
         GRID_HORIZONTAL = Line(Point(0, i), Point(WIDTH, i))
         GRID_HORIZONTAL.setOutline(color_rgb(25, 25, 25))
         GRID_HORIZONTAL.draw(WIN)
+
 
 def UI(WIDTH, HEIGHT):
     USER = Rectangle(Point(0, 400), Point(WIDTH, HEIGHT))
     USER.setFill(color_rgb(113, 191, 46))
     USER.setOutline(color_rgb(113, 191, 46))
     USER.draw(WIN)
+
+
+def Score_Counter_and_Display(SCORE):
+    COUNTER = Text(Point(75, 435), f"Score: {SCORE}")
+    COUNTER.setSize(20)
+    COUNTER.setFill("White")
+    COUNTER.draw(WIN)
+    return COUNTER
+
 
 def limits(X_LIM1, Y_LIM1, X_LIM2, Y_LIM2):
     if(X_LIM1 >= WIDTH or X_LIM1 <= 0):
@@ -55,100 +62,124 @@ def limits(X_LIM1, Y_LIM1, X_LIM2, Y_LIM2):
     else:
         return True
 
-def Snek_Reward(WIDTH, GRID_HEIGHT):
 
-    #Reward coordinates:
+def Snek_Reward(WIDTH, GRID_HEIGHT):
     REWARD_X = randrange(20, WIDTH - 20, 20)
     REWARD_Y = randrange(20, GRID_HEIGHT - 20, 20)
 
-    PRIZE = Rectangle(Point(REWARD_X, REWARD_Y), Point(REWARD_X + 20, REWARD_Y + 20))
+    PRIZE = Rectangle(Point(REWARD_X, REWARD_Y),
+                      Point(REWARD_X + 20, REWARD_Y + 20))
     PRIZE.setFill("Red")
     PRIZE.draw(WIN)
     return PRIZE
 
 
-PLAYER = {}
-PLAYER[0] = Rectangle(Point(X - 20 - RADIUS, Y - RADIUS), Point(X - 20 + RADIUS, Y + RADIUS))
+def main():
 
-j = 0
+    global FRAMES, SCORE, X, Y, BODY, RADIUS, LENGTH, PLAYER_LENGTH, DIRECTION, GAME, OVER, SPAWN, REWARD
 
-grid(WIDTH, GRID_HEIGHT)
-UI(WIDTH, HEIGHT)
+    # Snake values to be drawn
+    PLAYER = {}
+    PLAYER[0] = Rectangle(Point(X - 20 - RADIUS, Y - RADIUS),
+                          Point(X - 20 + RADIUS, Y + RADIUS))
 
-while GAME:
-    #Makes existing and potential new body
-    if len(PLAYER) < PLAYER_LENGTH:
-            i += 1
-            PLAYER[i] = PLAYER[i - 1].clone()
+    # Playing field
+    grid(WIDTH, GRID_HEIGHT)
+    UI(WIDTH, HEIGHT)
+    STREAK = Score_Counter_and_Display(SCORE)
 
-    #Makes body follow
-    PLAYER[0].undraw()
-    for i in range(1, len(PLAYER)):
-        PLAYER[len(PLAYER) - i].undraw()
-        PLAYER[len(PLAYER) - i] = PLAYER[len(PLAYER) - i - 1].clone()
-        PLAYER[len(PLAYER) - i].draw(WIN)
+    # Running game:
+    while GAME:
+        STREAK.undraw()
+        STREAK = Score_Counter_and_Display(SCORE)
 
-    #Head coordinates of snake
-    PLAYER[0] = Rectangle(Point(X - RADIUS, Y - RADIUS), Point(X + RADIUS, Y + RADIUS))
-    PLAYER[0].setOutline("White")
-    PLAYER[0].setFill("Cyan")
-    PLAYER[0].setWidth(2)
-    PLAYER[0].draw(WIN)
+        # Makes existing and potential new body
+        if len(PLAYER) < PLAYER_LENGTH:
+            BODY += 1
+            PLAYER[BODY] = PLAYER[BODY - 1].clone()
 
-    #Screen edges
-    GAME = True if limits(PLAYER[0].getP1().getX(), PLAYER[0].getP1().getY(),
-    PLAYER[0].getP2().getX(), PLAYER[0].getP2().getY()) else OVER
-
-    if(GAME == OVER):
+        # Makes body follow
         PLAYER[0].undraw()
-        PLAYER[0].setFill(color_rgb(220, 20, 60))
+        for i in range(1, len(PLAYER)):
+            PLAYER[len(PLAYER) - i].undraw()
+            PLAYER[len(PLAYER) - i] = PLAYER[len(PLAYER) - i - 1].clone()
+            PLAYER[len(PLAYER) - i].draw(WIN)
+
+        # Head coordinates of snake
+        PLAYER[0] = Rectangle(Point(X - RADIUS, Y - RADIUS),
+                          Point(X + RADIUS, Y + RADIUS))
+        PLAYER[0].setOutline("White")
+        PLAYER[0].setFill("Cyan")
         PLAYER[0].setWidth(2)
         PLAYER[0].draw(WIN)
 
-    for i in range(1, len(PLAYER)):
-        if(PLAYER[0].getCenter().getX() == PLAYER[i].getCenter().getX() and PLAYER[0].getCenter().getY() == PLAYER[i].getCenter().getY()):
+        # Screen edges
+        EDGE1 = PLAYER[0].getP1().getX()
+        EDGE2 = PLAYER[0].getP1().getY()
+
+        EDGE3 = PLAYER[0].getP2().getX()
+        EDGE4 = PLAYER[0].getP2().getY()
+
+        GAME = True if limits(EDGE1, EDGE2, EDGE3, EDGE4) else OVER
+
+        # Game over if edges are touched
+        if(GAME == OVER):
             PLAYER[0].undraw()
             PLAYER[0].setFill(color_rgb(220, 20, 60))
             PLAYER[0].setWidth(2)
             PLAYER[0].draw(WIN)
-            GAME=OVER
 
-    #User controls
-    TEMP = WIN.checkKey()
-    if TEMP == "Up":
-        DIRECTION = "Up"
-    elif TEMP == "Left":
-        DIRECTION = "Left"
-    elif TEMP == "Down":
-        DIRECTION = "Down"
-    elif TEMP == "Right":
-        DIRECTION = "Right"
+        # Game over if head touches body
+        for i in range(1, len(PLAYER)):
+            if(PLAYER[0].getCenter().getX() == PLAYER[i].getCenter().getX() and PLAYER[0].getCenter().getY() == PLAYER[i].getCenter().getY()):
+                PLAYER[0].undraw()
+                PLAYER[0].setFill(color_rgb(220, 20, 60))
+                PLAYER[0].setWidth(2)
+                PLAYER[0].draw(WIN)
+                GAME = OVER
 
-    if DIRECTION == "Up":
-        Y -= LENGTH
-    elif DIRECTION == "Left":
-        X -= LENGTH
-    elif DIRECTION == "Down":
-        Y += LENGTH
-    elif DIRECTION == "Right":
-        X += LENGTH
+        # User controls
+        TEMP = WIN.checkKey()
+        if TEMP == "Up":
+            DIRECTION = "Up"
+        elif TEMP == "Left":
+            DIRECTION = "Left"
+        elif TEMP == "Down":
+            DIRECTION = "Down"
+        elif TEMP == "Right":
+            DIRECTION = "Right"
 
-    #Snake objective spawning
-    if(SPAWN == False):
-        OBJECTIVE = Snek_Reward(WIDTH, GRID_HEIGHT)
-        SPAWN = True
+        if DIRECTION == "Up":
+            Y -= LENGTH
+        elif DIRECTION == "Left":
+            X -= LENGTH
+        elif DIRECTION == "Down":
+            Y += LENGTH
+        elif DIRECTION == "Right":
+            X += LENGTH
 
-    #If objective is reached, update values
-    CENTER1 = OBJECTIVE.getCenter().getX() == PLAYER[0].getCenter().getX()
-    CENTER2 = OBJECTIVE.getCenter().getY() == PLAYER[0].getCenter().getY()
-    
-    if(CENTER1 and CENTER2):
-        SPAWN = False
-        PLAYER_LENGTH += 1
-        OBJECTIVE.undraw()
+        # Snake objective spawning
+        if(SPAWN == False):
+            OBJECTIVE = Snek_Reward(WIDTH, GRID_HEIGHT)
+            SPAWN = True
 
-    #Rest goes here
-    update(10)
+        # If objective is reached, update values
+        CENTER1 = OBJECTIVE.getCenter().getX() == PLAYER[0].getCenter().getX()
+        CENTER2 = OBJECTIVE.getCenter().getY() == PLAYER[0].getCenter().getY()
 
-WIN.getMouse()
-WIN.close()
+        # Creates new reward, speeds up game and updates score
+        if(CENTER1 and CENTER2):
+            SPAWN = False
+            SCORE += 1
+            PLAYER_LENGTH += 1
+            if(FRAMES < 10):
+                FRAMES += 1
+            OBJECTIVE.undraw()
+
+        # Controls Snake speed
+        update(FRAMES)
+
+    WIN.close()
+
+if __name__ == '__main__':
+    main()
